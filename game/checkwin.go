3e903d1e82340dca158a6770b2173346a5ef25d9
Bruce Ediger <bediger8@gmail.com>
2019-11-07 09:47:11 -0700

    Check for win in subboard of last move

--- game/checkwin.go
+++ game/checkwin.go
@@ -6,8 +6,8 @@ var winningTriplets [][3]int = [][3]int{
 	[3]int{0, 4, 8}, [3]int{2, 4, 6},
 }
 
-func (b *Board) CheckWin(lastSlot int) bool {
-	subboard := 10 * (lastSlot % 10)
+func (b *Board) CheckWin(slot int) bool {
+	subboard := slot - (slot % 10)
 	for _, triplet := range winningTriplets {
 		n := b.Cell[subboard+triplet[0]] + b.Cell[subboard+triplet[1]] + b.Cell[subboard+triplet[2]]
 		if n == 3 || n == -3 {
