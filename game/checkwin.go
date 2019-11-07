015e63fb9b8ac2a3418921aadd25470382353ef8
Bruce Ediger <bediger8@gmail.com>
2019-11-06 22:38:55 -0700

    first checkin

---
+++ game/checkwin.go
@@ -0,0 +1,18 @@
+package game
+
+var winningTriplets [][3]int = [][3]int{
+	[3]int{0, 1, 2}, [3]int{3, 4, 5}, [3]int{6, 7, 8},
+	[3]int{0, 3, 6}, [3]int{1, 4, 7}, [3]int{2, 5, 8},
+	[3]int{0, 4, 8}, [3]int{2, 4, 6},
+}
+
+func (b *Board) CheckWin(lastSlot int) bool {
+	subboard := 10 * (lastSlot % 10)
+	for _, triplet := range winningTriplets {
+		n := b.Cell[subboard+triplet[0]] + b.Cell[subboard+triplet[1]] + b.Cell[subboard+triplet[2]]
+		if n == 3 || n == -3 {
+			return true
+		}
+	}
+	return false
+}
