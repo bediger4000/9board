a57d1e39382bf3f1a5c6721fab9823481b7af248
Bruce Ediger <bediger8@gmail.com>
2019-11-14 14:18:54 -0700

    remove debug printfs

--- main.go
+++ main.go
@@ -8,14 +8,20 @@ import (
 func main() {
 	bd := &game.Board{}
 
-	moveSlot := 44
+	moveSlot := -1
+
 	for {
 		fmt.Printf("%s\n", bd)
 		moveSlot = game.ReadMove(bd, moveSlot)
-		fmt.Printf("Read %d\n", moveSlot)
 		bd.MakeMove(moveSlot, game.Min)
 		if bd.CheckWin(moveSlot) {
-			fmt.Printf("Win! with %d\n", moveSlot)
+			fmt.Printf("Human wins\n")
+			break
+		}
+		moveSlot = uct.FindMove(bd, 100000)
+		bd.MakeMove(moveSlot, game.Max)
+		if bd.CheckWin(moveSlot) {
+			fmt.Printf("Computer wins\n")
 			break
 		}
 	}
